7da220671566449658027ffcc0f779e0fa654d78
Arlindo Garcia <arlindogarciagomesneto70@gmail.com>
2022-09-28 23:39:53 -0300

    Feat: server, cors, classe de tratamento de erros

---
+++ src/shared/http/app.ts
@@ -0,0 +1,50 @@
+import 'reflect-metadata';
+import 'dotenv/config';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import AppError from '@shared/errors/AppError';
+import { errors } from 'celebrate';
+
+class App {
+  public server: Express;
+
+  constructor() {
+    this.server = express();
+
+    this.config();
+    this.middlewaresErrors();
+  }
+
+  config() {
+    this.server.use(cors());
+    this.server.use(express.json());
+  }
+
+  middlewaresErrors() {
+    this.server.use(errors());
+
+    this.server.use(
+      (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction,
+      ) => {
+        if (error instanceof AppError) {
+          return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message,
+          });
+        }
+
+        return response.status(500).json({
+          status: 'error',
+          message: 'Internal server error',
+        });
+      },
+    );
+  }
+}
+
+export default new App().server;
---
+++ src/shared/http/server.ts
@@ -0,0 +1,5 @@
+import app from './app';
+
+app.listen(3333, () => {
+  console.log('Server starded on port 3333!');
+});
